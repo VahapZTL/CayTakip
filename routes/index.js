537ef315e8792faaf673e195b729a92eecd435d6
Abdulvahap ÖZTEL <vahapoztel61@hotmail.com>
2017-09-06 16:03:17 +0300

    Routers was updated.

--- routes/index.js
+++ routes/index.js
@@ -8,71 +8,8 @@ var User = mongoose.model('User');
 
 var config = require('../config/config');
 
-
-//Login Router
-router.post('/login', function(req, res) {
-    User.findOne({email: req.body.email}, function (err, user) {
-        if(err)
-            throw err;
-        if(!user)
-            console.log('Kullanıcı bulunamadı.');
-        if(user){
-            user.verifyPassword(req.body.password, function (err, isMatch) {
-                if(err){
-                    res.status(500).json({
-                        success: false,
-                        data: err
-                    });
-                }else if(!isMatch){
-                    res.json({
-                        success: false,
-                        data: 'Girdiğiniz bigileri kontrol edin!'
-                    });
-                }else if(isMatch){
-                    var token = jwt.sign({ _id : user._id }, config.Secret);
-                    user.token = token;
-
-                    user.save(function (err) {
-                        if(err)
-                            throw err;
-                        console.log('Token kaydı başarılı.');
-                    });
-
-                    res.json({
-                        success: true,
-                        data: token
-                    });
-                }
-            });
-        }
-    });
-});
-
-//Register Router
-router.post('/register', function (req, res) {
-    User.findOne({email: req.body.email}, function (err, user) {
-        if(err)
-            throw err;
-        if(user){
-            res.json({
-                data: 'Bu email adresi kullanılıyor.'
-            });
-        } else{
-            var userModel = new User();
-            userModel.email = req.body.email;
-            userModel.password = userModel.generateHash(req.body.password);
-
-            userModel.save(function (err) {
-                if(err)
-                    throw err;
-                console.log('Kayıt Başarılı');
-            });
-        }
-    });
-});
-
 //Authorization Function
-isAuth = function (req, res, next) {
+function isAuth(req, res, next) {
     //Coming token from headers
     var comingToken = req.headers["authorization"];
 
@@ -100,5 +37,99 @@ isAuth = function (req, res, next) {
         });
     }
 };
+
+//Login Router
+router.post('/login', function(req, res) {
+    User.findOne({email: req.body.email}, function (err, user) {
+        if(err){
+            res.status(500).json({
+                success: false,
+                data: err
+            });
+        }else{
+            if(user){
+                user.validPassword(req.body.password, function (err, isMatch) {
+                    if(err){
+                        res.status(500).json({
+                            success: false,
+                            data: err
+                        });
+                    }else if(!isMatch){
+                        res.json({
+                            success: false,
+                            data: 'Girdiğiniz bigileri kontrol edin!'
+                        });
+                    }else if(isMatch){
+                        var token = jwt.sign({ _id : user._id }, config.Secret);
+                        user.token = token;
+
+                        res.json({
+                            success: true,
+                            data: token
+                        });
+                    }
+                });
+            }else{
+                res.json({
+                    success: false,
+                    data: 'Girilen bilgiler yanlış yada böyle bir kullanıcı yok!'
+                });
+            }
+        }
+    });
+});
+
+//Register Router
+router.post('/register', function (req, res) {
+    User.findOne({email: req.body.email}, function (err, user) {
+        if(err)
+            throw err;
+        if(user){
+            res.json({
+                data: 'Bu email adresi kullanılıyor.'
+            });
+        } else{
+            var userModel = new User();
+            userModel.email = req.body.email;
+            userModel.password = userModel.generateHash(req.body.password);
+
+            userModel.save(function (err) {
+                if(err)
+                    throw err;
+                res.json({
+                    success: true,
+                    data: 'Kayıt Başarılı!'
+                });
+            });
+        }
+    });
+});
+
+//Surgun Create Router
+router.post('/createSurgun', isAuth, function (req, res) {
+    User.findOne({_id: req.decoded}, function (err, user) {
+        if(err)
+            throw err;
+        if(!user)
+            console.log('Kullanıcı bulunamadı!');
+
+        var surgunModel = new Surgun();
+        surgunModel.surgunNumarasi = req.body.surgunNumarasi;
+
+        user.surgun = surgunModel._id;
+
+        user.save(function (err) {
+            if(err)
+                throw err;
+            console.log('Surgun ID kaydedildi!');
+        });
+
+        surgunModel.save(function (err) {
+            if(err)
+                throw err;
+            console.log('Surgun Modeli kaydedildi!');
+        });
+    });
+});
 
 module.exports = router;
